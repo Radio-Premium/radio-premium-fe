fe5ec4069dbf5c2651ca2a63c8e69e6b277a94e7
devjaeyoon <devjaeyoon@gmail.com>
2025-06-17 19:13:09 +0900

    feat: ErrorBoundary 컴포넌트 구현 및 unhandledrejection 핸들링 추가
    React ErrorBoundary 클래스 컴포넌트로 하위 컴포넌트 트리에서 발생하는
    자바스크립트 에러와 비동기 예외(unhandledrejection)를 포착하여
    Fallback UI를 제공하고, 에러 상태를 리셋할 수 있는 기능 구현

---
+++ src/shared/components/ErrorBoundary.jsx
@@ -0,0 +1,44 @@
+import * as React from "react";
+
+export class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+    this.captureReject = this.captureReject.bind(this);
+    this.resetError = this.resetError.bind(this);
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  captureReject(e) {
+    e.preventDefault();
+    console.log("dd");
+    this.setState({ hasError: true });
+  }
+
+  componentDidMount() {
+    window.addEventListener("unhandledrejection", this.captureReject);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("unhandledrejection", this.captureReject);
+  }
+
+  resetError() {
+    this.setState({ hasError: false });
+    this.props.onReset?.();
+  }
+
+  render() {
+    const { fallback: Fallback, children } = this.props;
+
+    if (this.state.hasError) {
+      console.log("ErrorBoundary에서 에러를 감지했습니다.");
+      return <Fallback resetError={this.resetError} />;
+    }
+
+    return children;
+  }
+}
