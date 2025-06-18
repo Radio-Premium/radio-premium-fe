68b9683b8ecb3e618f29c971541a050eb0cd206b
h-faber <hfaberlu@gmail.com>
2025-06-18 23:05:49 +0900

    fix: 광고 제보 모달 타이틀이 노출되지 않는 현상 수정

--- src/AdReport/constants/messages.js
+++ src/AdReport/constants/messages.js
@@ -1,6 +1,6 @@
 export const AD_REPORT_MESSAGES = {
-  TITLE: "현재 방송이 광고인지 선택해주세요.",
-  SUBTITLE: "광고일 경우, 채널을 변경할 수 있습니다.",
+  MODAL_TITLE: "현재 방송이 광고인지 선택해주세요.",
+  MODAL_SUBTITLE: "광고일 경우, 채널을 변경할 수 있습니다.",
   INPUT_LABEL: "📌 광고로 판단되는 멘트를 입력해주세요 (선택)",
   INPUT_PLACEHOLDER: "EX) '하핑하핑'",
   BUTTON_CANCEL: "취소",
